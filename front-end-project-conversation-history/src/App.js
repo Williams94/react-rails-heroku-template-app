d728a9e411f3e87d3e847d9c605c664a7c22c8a0
Ross Williams <r.b.williams94@gmail.com>
2023-03-06 17:59:30 +0000

    Added material-ui for styling and allowing user to create a new message

--- front-end-project-conversation-history/src/App.js
+++ front-end-project-conversation-history/src/App.js
@@ -1,49 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import Moment from 'moment';
+import { List, Divider, ListItem, ListItemText } from '@material-ui/core';
+
 import './App.css';
 
 const App = () => {
+  Moment.locale('en');
   const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-    useEffect(() => {
-      fetch('http://localhost:3001/messages')
-        .then(response => response.json())
-        .then(data => setMessages(data));
-    }, []);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/messages', {
-  //     method: 'POST',
-  //     mode: 'cors',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       'message': {
-  //         'message_body': 'Hello, world!'
-  //       }
-  //     })
-  //     // body: {
-  //       // message_body: 'Hello, world!'
-  //     // }
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-  //     .catch(error => console.error(error))
-  // }, []);
+  useEffect(() => {
+    fetch('http://localhost:3001/messages')
+      .then(response => response.json())
+      .then(data => setMessages(data));
+  }, []);
 
+  const handleButtonClick = () => {
+    fetch('http://localhost:3001/messages', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'message': {
+          'message_body': inputValue
+        }
+      })
+    })
+      .then(response => response.json())
+      .then(data => setMessages([ ...messages, data ]))
+      .catch(error => console.error(error))
+  };
 
   return (
     <div className="App">
-      <ul>
+      <h1>Project Conversation History</h1>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#E7EBF0' }}>
+        <ListItem key="header">
+          <ListItemText primary="Message:" />
+          <ListItemText primary="Sent:" />
+        </ListItem>
+        <Divider component="li" />
         {messages.map(message => (
           <>
-            <li key={message.id}>{message.message_body}</li>
-            <li key={message.id}>{message.created_at}</li>
+            <ListItem key={message.id}>
+              <ListItemText primary={message.message_body} />
+              <ListItemText primary={Moment(message.created_at).format('DD MMM YYYY')} />
+            </ListItem>
+            <Divider component="li" />
           </>
         ))}
-      </ul>
+      </List>
+      <div>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleButtonClick}>Log Input Value</button>
+      </div>
     </div>
   );
 }
